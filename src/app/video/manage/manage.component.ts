23f517b264d2ec76cc26bdff94ced77912505415
Moiz <donwebrockin@hotmail.com>
2022-05-31 13:13:07 +0100

    Added edit modal

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
@@ -12,7 +13,8 @@ export class ManageComponent implements OnInit {
   clips:IClip[] = []
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
     ) { }
 
   ngOnInit(): void {
@@ -39,6 +41,12 @@ export class ManageComponent implements OnInit {
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clip: IClip){
+    $event.preventDefault()
+    this.modal.toggleModal('editClip')
+
   }
 
 }
